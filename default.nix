3aef8297cb20ff9d0e4d80a70cb9c36b4466d9f2
knupfer <fknupfer@gmail.com>
2019-10-04 15:22:01 +0200

    clean up

--- default.nix
+++ default.nix
@@ -1,5 +1,7 @@
 { project, directory, versions }:
 
+with builtins;
+
 let
 
   historic = import ((import <nixpkgs> {}).fetchFromGitHub {
@@ -8,13 +10,13 @@ let
     rev    = "685f67412975a4669b0867d4bbf95e86eceebffc";
     sha256 = "0v92b9209h8z6fw4yd4q5bxnv2swcmvwwhg74lp3vy8gk9pnjjki";});
 
-  pkgs = (versionTuple (builtins.head versions)).value;
-  ghc = (versionTuple (builtins.head versions)).name;
+  pkgs = (versionTuple (head versions)).value;
+  ghc = (versionTuple (head versions)).name;
 
-  versionTuple = x: if builtins.isAttrs x
+  versionTuple = x: if isAttrs x
                     then
-                      { name = versionToGhc (builtins.head (builtins.attrNames x));
-                        value = builtins.head (builtins.attrValues x);
+                      { name = versionToGhc (head (attrNames x));
+                        value = head (attrValues x);
                       }
                     else
                       { name = versionToGhc x;
@@ -24,11 +26,11 @@ let
     if xs == []
     then abort "Following ghc version could not be found: ${v}"
     else
-      if historic.${builtins.head xs}.haskell.packages ? ${v}
-      then builtins.head xs
-      else latestPkgs (builtins.tail xs) v;
-  listOfNixpkgs = builtins.sort (a: b: builtins.lessThan b a) (builtins.attrNames historic);
-  versionToGhc = x: "ghc" + builtins.replaceStrings ["."] [""] x;
+      if historic.${head xs}.haskell.packages ? ${v}
+      then head xs
+      else latestPkgs (tail xs) v;
+  listOfNixpkgs = sort (a: b: lessThan b a) (attrNames historic);
+  versionToGhc = x: "ghc" + replaceStrings ["."] [""] x;
   eval = g: p: with historic.${p};
     ( if haskell.lib ? doBenchmark then haskell.lib.doBenchmark else x: x)
       ( haskell.packages.${g}.callCabal2nix project
@@ -51,7 +53,7 @@ if release
 then
 
    { archive = historic.${pkgs}.haskell.lib.sdistTarball (eval ghc pkgs);
-   } // (historic.${pkgs}.lib.mapAttrs (x: y: addGhcVersion (eval x y) x) (builtins.listToAttrs (builtins.map versionTuple versions)))
+   } // (historic.${pkgs}.lib.mapAttrs (x: y: addGhcVersion (eval x y) x) (listToAttrs (map versionTuple versions)))
 
 else
 
